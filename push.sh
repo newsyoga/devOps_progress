20258da117f6e9ee404c8171b85fcc767c53a2cd
Shantanu Pawar <shpawar410@gmail.com>
2024-12-08 05:43:47 +0530

    updated python_learnings c2 Sun Dec  8 05:43:47 IST 2024

--- push.sh
+++ push.sh
@@ -2,5 +2,10 @@
 read -p "Enter git commit message : " msgStr
 
 git add *
-git commit -m "$(date) $msgStr"
+echo "git add done \n\n"
+
+git commit -m "$msgStr $(date)"
+echo "git commit done : $msgStr $(date)\n\n"
+
 git push
+echo "done upload exiting script...\n\n"
